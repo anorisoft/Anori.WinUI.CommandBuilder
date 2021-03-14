468d0553af3deb52893983dc10c1d4e0f0f10bd5
Martin Egli <martin.egli@reishauer.com>
2021-03-14 19:21:19 +0100

    Update

--- Submodules/Add Submodules.cmd
+++ Submodules/Add Submodules.cmd
@@ -1,3 +1,4 @@
 git submodule add -b develop https://github.com/anorisoft/Anori.Extensions Anori.Extensions
 git submodule add -b develop https://github.com/anorisoft/Anori.ExpressionObservers Anori.ExpressionObservers
+git submodule add -b develop https://github.com/anorisoft/Anori.Common Anori.Common
 pause
